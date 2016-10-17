e7c40f1434145b993342c7dbc772b382dd772528
petre2dor <petre.tudor@protonmail.com>
2016-10-17 23:29:48 +0200

    start using mcpadc #30

--- devices/controller/Temperature.js
+++ devices/controller/Temperature.js
@@ -1,6 +1,6 @@
 var LogModel    = require('../../db_models/LogModel.js')
 var DeviceModel = require('../../db_models/DeviceModel')
-var PythonShell = require('python-shell')
+var Mcpadc      = require('mcp-spi-adc')
 
 // PythonShell.defaultOptions = { scriptPath: '' }
 
@@ -8,44 +8,33 @@ exports.get = function(req, res) {
     var deviceModel = new DeviceModel()
     deviceModel.setId(req.params.deviceId)
     deviceModel.read()
-        .then(() => {
-            return callReadTempPyScript('scripts/temperature/read_tmp36.py')
+    .then(() => {
+        return readTemperature()
+    })
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(e => {
+        res.status(404).json({
+            httpCode: 404,
+            type: 'ERROR',
+            message: e.message,
+            data: e
         })
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(e => {
-            res.status(404).json({
-                httpCode: 404,
-                type: 'ERROR',
-                message: e.message,
-                data: e
-            })
-        })
+    })
 }
 
-getRandomTemperature = function(){
-    return Math.floor(Math.random() * (35 - 15) + 15)
-}
-
-callReadTempPyScript = function(scriptPath)
-{
+readTemperature = () => {
     return new Promise((resolve, reject) => {
-        PythonShell.run(scriptPath, function (err, results) {
-            if (err) {
-                reject({ httpCode: 403, type: 'ERROR', message: err.message, data: err })
-            } else {
-                if(isNaN(parseFloat(results))) {
-                    reject({ httpCode: 403, type: 'ERROR', message: 'Temperature '+scriptPath+' script failed', data: {} })
-                } else {
-                    resolve({
-                        httpCode: 200,
-                        type: 'SUCCESS',
-                        message: 'Here is the temperature.',
-                        data: {temperature: parseFloat(results)}
-                    })
-                }
-            }
+        var tempSensor = mcpadc.open(0, {speedHz: 20000}, err => {
+            if (err) reject({ type: 'ERROR', message: 'Could not open mcpadc.', httpCode: 400, data: err })
+
+            tempSensor.read((err, reading) => {
+                if (err) reject({ type: 'ERROR', message: 'Could not read mcpadc.', httpCode: 400, data: err })
+
+                var temperature = (reading.value * 3.3 - 0.5) * 100;
+                resolve({ message: 'Here is the temperature.', type: 'SUCCESS', httpCode: 200, data: {temperature: temperature} })
+            })
         })
     })
 }
