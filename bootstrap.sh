7ac6d9615414f739cb6e46c936f90c3acf3a02c3
Remus CURSARU <remus.cm@gmail.com>
2016-11-01 09:55:29 +0200

    smarter bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,13 @@
 # bootstrap installer for this project
 # it clones the repo from Github and runs the initial steps
 
-mkdir /piGarden
-git clone https://github.com/petre2dor/piGarden.git /piGarden
-/piGarden/script/createUser.sh
-/piGarden/script/patch.sh
+if [ ! -d /piGarden ]; then
+    mkdir /piGarden
+    git clone https://github.com/petre2dor/piGarden.git /piGarden
+    /piGarden/script/createUser.sh
+    /piGarden/script/patch.sh
 
-echo -e "run \n sudo su gradinar\n to switch the the owner account"
+    echo -e "run \nsu gradinar\n to switch the the owner account"
+else
+    echo -e "/piGarden exists, maybe you should try running \nsu gradinar && cd /piGarden && git pull\n to update it"
+fi
