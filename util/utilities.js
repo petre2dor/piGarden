4c8fcfd2028c59a759eb29b92f3e6735ddfabcd4
petre2dor <petre.tudor@protonmail.com>
2016-11-11 23:51:46 +0100

    added util map_range method

--- util/utilities.js
+++ util/utilities.js
@@ -5,6 +5,11 @@ class Utilities {
     {
         return str && str[0].toUpperCase() + str.slice(1);
     }
+
+    static map_range(value, low1, high1, low2, high2)
+    {
+        return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+    }
 }
 
 module.exports = Utilities
