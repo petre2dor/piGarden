07925a16dfca029c913cfb6d71e401905d66f141
Petre Tudor <petre.tudor@protonmail.com>
2017-02-24 15:13:56 +0200

    read sht10 using binary

--- devices/controller/sht1x/read.js
+++ devices/controller/sht1x/read.js
@@ -1,17 +1,55 @@
-var PythonShell = require('python-shell')
+const os = require('os')
+const os = require('os')
 
 exports.read = deviceOptions => {
-    return new Promise((resolve, reject) => {
-        PythonShell.run('controller/sht1x/read.py', {args: [deviceOptions.dataPin, deviceOptions.clockPin]}, (err, results) => {
-            let result = err
-                            ? { httpCode: 403, type: 'ERROR', message: err.message, data: err }
-                            : JSON.parse(results[0])
+        APICall = spawn('sht', ['-v', '-trd', deviceOptions.clockPin, deviceOptions.dataPin]);
+        if (typeof APICall != "undefined") {
+            APICall.stdout.on('data', data => {
+                console.log('backend: ' + data)
+                APIOutput += data
+            });
+            APICall.stderr.on('data', data => {
+                console.log('backend: ' + data)
+                APIOutput += data
+            });
 
-            if(result.httpCode >= '400') {
-                reject({ httpCode: 403, type: 'ERROR', message: 'Read sht1x script failed', data: result.message })
-            } else {
-                resolve(result)
-            }
-        })
-    })
+            return new Promise((resolve, reject) => {
+                APICall.on('close', code => {
+                    console.log('exitcode: ' + code)
+                    console.log('output: ' + APIOutput)
+
+                    resolve({ httpCode: 200, type: 'SUCCESS', data: getDataFromOutput(APIOutput), message: 'Read sht10 successfuly from dataPin: '+deviceOptions.dataPin+', clockPin: '+deviceOptions.clockPin)
+                });
+                APICall.on('error', code => {
+                    console.log('errorcode: ' + code)
+                    reject({
+                            httpCode: 403,
+                            type: 'ERROR',
+                            message: 'Error reading sht10 from dataPin: '+deviceOptions.dataPin+', clockPin: '+deviceOptions.clockPin,
+                            data: 'exitcode: ' + code + ' output: ' + APIOutput
+                        })
+                });
+            })
+        }
+}
+
+
+getDataFromOutput = output => {
+    let multiline = output.split(os.EOL)
+    multiline.forEach(element => {
+        let line = element.split(':')
+        switch (line[0].trim()) {
+            case 'temperature':
+                let temperature = line[1].trim()
+                break;
+            case 'rh':
+                let rh = line[1].trim()
+                break;
+            case 'dew_point':
+                let dew_point = line[1].trim()
+                break;
+        }
+    });
+
+    return [{"type": "TEMPERATURE", "value": temperature}, {"type": "HUMIDITY", "value": rh}, {"type": "DEW", "value": dew_point}]
 }
--- devices/controller/sht1x/read_dev.js
+++ devices/controller/sht1x/read_dev.js
@@ -1,19 +1,6 @@
-var PythonShell = require('python-shell')
 
 exports.read = deviceOptions => {
     return new Promise((resolve, reject) => {
-        PythonShell.run('controller/sht1x/read_dev.py', {args: [deviceOptions.dataPin, deviceOptions.clockPin]}, (err, results) => {
-            if (err) {
-                reject({ httpCode: 403, type: 'ERROR', message: err.message, data: err })
-            } else {
-                let result = JSON.parse(results[0])
-
-                if(result.httpCode >= '400') {
-                    reject({ httpCode: 403, type: 'ERROR', message: 'Read dht22 script failed', data: result.message })
-                } else {
-                    resolve(result)
-                }
-            }
-        })
+        resolve({httpCode: 200, type: 'SUCCESS', data: [{"type": "TEMPERATURE", "value": "22"}, {"type": "HUMIDITY", "value": "45"}, {"type": "DEW", "value": "8"}], message: 'Read sht10 successfuly from dataPin: '+deviceOptions.dataPin+', clockPin: '+deviceOptions.clockPin})
     })
 }
