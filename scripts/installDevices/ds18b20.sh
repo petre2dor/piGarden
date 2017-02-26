6f2e307ba668dd4442a2c5168a8b371fe3bffbf2
Remus CURSARU <remus.cm@gmail.com>
2017-02-26 15:04:53 +0200

    fixed ds18b20 install script

---
+++ scripts/installDevices/ds18b20.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+if [[ "$EUID" -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   echo "please run: sudo $(readlink -f $0) or sudo !!"
+   exit 1
+fi
+echo 'w1-gpio' >> /etc/modules
+echo 'w1-therm' >> /etc/modules
+echo 'dtoverlay=w1-gpio' >> /boot/config.txt
+
+. /etc/environment
+cd $PI_GARDEN_ROOT
+npm install ds18b20-raspi
--- scripts/installDevices/install_DS18B20.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-if [[ "$EUID" -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   echo "please run: sudo $(readlink -f $0) or sudo !!"
-   exit 1
-fi
-
-
-install npm install --save ds18b20-raspi
