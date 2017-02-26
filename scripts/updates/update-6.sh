273b136543f66cf9d7716403fc4b96ebe3ef66b2
Remus CURSARU <remus.cm@gmail.com>
2017-02-26 12:48:28 +0200

    fixed grafana and influxdb installers

--- scripts/updates/update-6.sh
+++ scripts/updates/update-6.sh
@@ -8,23 +8,24 @@ case `uname -m` in
         echo 'x86_64'
         LOCAL_DEB="$PI_GARDEN_ROOT/installers/influxdb/influxdb_1.2.0_amd64.deb"
         dpkg -i $LOCAL_DEB
+        apt-get update
+        apt-get -fy install
         ;;
     armv7l)
         echo 'armv7 (raspberry)'
-        LOCAL_DEB="$PI_GARDEN_ROOTinstallers/influxdb/influxdb-1.2.0_linux_armhf.tar.gz"
-        tar xvfz influxdb-1.2.0_linux_armhf.tar.gz --transform="s|./influxdb-1.2.0-1||" --show-transformed-names
-        systemctl enable influxdb
+        LOCAL_TAR="$PI_GARDEN_ROOT/installers/influxdb/influxdb-1.2.0_linux_armhf.tar.gz"
+        tar xvfzP $LOCAL_TAR --transform="s|./influxdb-1.2.0-1||" --show-transformed-names
+        cp /usr/lib/influxdb/scripts/influxdb.service /etc/systemd/system/influxdb.service
+        useradd influxdb -s /bin/false -m
+        chown -R influxdb /var/lib/influxdb/
         ;;
     *)
         echo 'wtf?!'
         exit 1
         ;;
 esac
-
-apt-get update
-apt-get -fy install
-
-chmod +x /etc/init.d/influxd
-service influxdb start
+systemctl daemon-reload
+systemctl enable influxdb
+systemctl start influxdb
 
 curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE piGarden"
