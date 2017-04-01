ab1cb6b4f95fd11ea2bd39007bbce654ae4a95ae
Petre Tudor <petre.tudor@protonmail.com>
2017-04-01 16:51:25 +0200

    use "systemd" instead of "ubuntu" when running  "pm2 startup"

--- scripts/updates/update-4.sh
+++ scripts/updates/update-4.sh
@@ -11,7 +11,7 @@ echo "************************************"
 
 sudo su $PI_GARDEN_USER_NAME -c 'pm2 list'
 chmod -R go+w "/home/$PI_GARDEN_USER_NAME/.pm2/"
-sudo su -c "env PATH=$PATH:/usr/bin/node pm2 startup ubuntu -u $PI_GARDEN_USER_NAME --hp /home/$PI_GARDEN_USER_NAME"
+sudo su -c "env PATH=$PATH:/usr/bin/node pm2 startup systemd -u $PI_GARDEN_USER_NAME --hp /home/$PI_GARDEN_USER_NAME"
 sudo su -c "env PATH=$PATH:/usr/bin/node pm2 logrotate -u $PI_GARDEN_USER_NAME"
 cd /piGarden
 sudo su $PI_GARDEN_USER_NAME -c "pm2 start $PI_GARDEN_ROOT/app.json"
