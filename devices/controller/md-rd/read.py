98395dcc169912d30a7d2fe4257b104cd3eba75a
Petre Tudor <petre2dor@users.noreply.github.com>
2017-04-01 17:15:06 +0000

    py script for reading rain detector

---
+++ devices/controller/md-rd/read.py
@@ -0,0 +1,23 @@
+import time
+import sys
+import RPi.GPIO as GPIO
+#set up GPIO using BCM numbering
+
+PIN = int(sys.argv[1])
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.IN)
+
+readTime = 3 #sec
+readEvery = 0.25 #sec
+timePassed = 0
+
+watherDetected = False
+while timePassed < readTime:
+        pin = GPIO.input(PIN)
+        if pin < 1:
+                watherDetected = True
+        timePassed += readEvery
+        time.sleep(readEvery)
+GPIO.cleanup(PIN)        
+print '{"httpCode": "200", "type": "SUCCESS", "message": "All good.", "data": {"type": "RAINING", "value": "'+str(int(watherDetected))+'"})'
